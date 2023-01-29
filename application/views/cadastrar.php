4bb58282686b378e20288dd0eb5ffeffb6c19a64
Alexandre Martins <alexandremartins12@hotmail.com>
2023-01-29 18:04:02 -0300

    Update cadastrar.php

--- application/views/cadastrar.php
+++ application/views/cadastrar.php
@@ -57,8 +57,8 @@
 				<input type="text" class="form-control telefones" name="celular" id="celular" placeholder="Digite o celular" maxlength="15">
 			</div>
 			<div class="form-group col-md-4">
-				<label for="endereco">Endereço</label>
-				<input type="text" class="form-control" name="endereco" id="endereco" placeholder="Digite o endereço" maxlength="45">
+				<label for="cep">CEP</label>
+				<input type="text" class="form-control" name="cep" id="cep" placeholder="Digite o cep" maxlength="10">
 			</div>
 			<div class="form-group col-md-4">
 				<label for="cidade">Cidade</label>
@@ -67,17 +67,17 @@
 		</div>
 
 		<div class="row">
-			<div class="form-group col-md-4">
+			<div class="form-group col-md-3">
 				<label for="estado">Estado/Província</label>
 				<input type="text" class="form-control" name="estado" id="estado" placeholder="Digite o estado/província" maxlength="45">
 			</div>
-			<div class="form-group col-md-4">
-				<label for="cep">CEP</label>
-				<input type="text" class="form-control" name="cep" id="cep" placeholder="Digite o cep" maxlength="10">
+			<div class="form-group col-md-6">
+				<label for="endereco">Endereço</label>
+				<input type="text" class="form-control" name="endereco" id="endereco" placeholder="Digite o endereço" maxlength="45">
 			</div>
-			<div class="form-group col-md-4">
+			<div class="form-group col-md-3">
 				<label for="pais">País/Região</label>
-				<input type="text" class="form-control" name="pais" id="pais" placeholder="Digite o país/região" maxlength="45">
+				<input type="text" class="form-control" name="pais" id="pais" placeholder="Digite o país/região" maxlength="45" value="Brasil">
 			</div>
 		</div>
 
@@ -102,3 +102,83 @@
   $('.telefones').mask(SPMaskBehavior, spOptions);
   $('#cep').mask('00.000-000');
 </script>
+
+ <!-- Adicionando JQuery -->
+ <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+            integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+            crossorigin="anonymous"></script>
+
+    <!-- Adicionando Javascript -->
+    <script>
+
+        $(document).ready(function() {
+
+            function limpa_formulário_cep() {
+                // Limpa valores do formulário de cep.
+                $("#endereco").val("");
+                $("#bairro").val("");
+                $("#cidade").val("");
+                $("#estado").val("");
+                $("#ibge").val("");
+            }
+            
+            //Quando o campo cep perde o foco.
+            $("#cep").blur(function() {
+
+                //Nova variável "cep" somente com dígitos.
+                var cep = $(this).val().replace(/\D/g, '');
+
+                //Verifica se campo cep possui valor informado.
+                if (cep != "") {
+
+                    //Expressão regular para validar o CEP.
+                    var validacep = /^[0-9]{8}$/;
+
+                    //Valida o formato do CEP.
+                    if(validacep.test(cep)) {
+
+                        //Preenche os campos com "..." enquanto consulta webservice.
+                        $("#endereco").val("...");
+                        $("#bairro").val("...");
+                        $("#cidade").val("...");
+                        $("#estado").val("...");
+                        $("#ibge").val("...");
+
+                        //Consulta o webservice viacep.com.br/
+                        $.getJSON("https://viacep.com.br/ws/"+ cep +"/json/?callback=?", function(dados) {
+
+                            if (!("erro" in dados)) {
+                                //Atualiza os campos com os valores da consulta.
+                                $("#endereco").val(dados.logradouro);
+                                $("#bairro").val(dados.bairro);
+                                $("#cidade").val(dados.localidade);
+                                $("#estado").val(dados.uf);
+                                $("#ibge").val(dados.ibge);
+                            } //end if.
+                            else {
+                                //CEP pesquisado não foi encontrado.
+                                limpa_formulário_cep();
+                                alert("CEP não encontrado.");
+                            }
+                        });
+                    } //end if.
+                    else {
+                        //cep é inválido.
+                        limpa_formulário_cep();
+                        alert("Formato de CEP inválido.");
+                    }
+                } //end if.
+                else {
+                    //cep sem valor, limpa formulário.
+                    limpa_formulário_cep();
+                }
+            });
+        });
+
+    </script>
+    
+
+ 
+
+   
+    
