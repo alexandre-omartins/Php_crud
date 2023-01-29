c097081d08c672c337c60308c69525fe8ad525bb
Alexandre Martins <alexandremartins12@hotmail.com>
2023-01-29 18:04:00 -0300

    Update routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'contatos';
+$route['default_controller'] = 'contatos/listar';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
